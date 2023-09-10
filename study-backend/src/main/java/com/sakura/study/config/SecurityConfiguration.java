52ad8513d54e862e2aa2223dbc710ebc29789a88
sakura <1234@gmail.com>
2023-09-10 14:47:08 +0800

    数据库配置&登陆验证接口

--- study-backend/src/main/java/com/sakura/study/config/SecurityConfiguration.java
+++ study-backend/src/main/java/com/sakura/study/config/SecurityConfiguration.java
@@ -11,9 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import java.io.IOException;
 
