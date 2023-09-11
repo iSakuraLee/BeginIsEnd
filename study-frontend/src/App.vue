5c111649249ca527e2bbe348bcd03b99354217d9
sakura <1234@gmail.com>
2023-09-11 21:31:52 +0800

    前端页面拆分组建

--- study-frontend/src/App.vue
+++ study-frontend/src/App.vue
@@ -1,53 +1,9 @@
 <script setup>
-import {Lock, User} from '@element-plus/icons-vue'
+
 </script>
 
 <template>
-  <div style="width: 100vw; height: 100vh;overflow: hidden;display: flex">
-    <div style="flex: 1;background-color: dodgerblue">
-      <el-image style="width: 100%;height: 100%" fit="cover" src="https://img1.baidu.com/it/u=2463514011,1142503686&fm=253&fmt=auto&app=138&f=JPEG?w=888&h=500"/>
-    </div>
-    <div style="width: 400px;background-color: white">
-      <div style="text-align: center;margin: 0 20px">
-        <div style="margin-top: 150px">
-          <div style="font-size: 25px">登陆</div>
-          <div style="font-size: 14px;color: grey">请先输入用户名密码进行登陆</div>
-        </div>
-        <div style="margin-top: 30px">
-          <el-input type="text" placeholder="用户名/邮箱">
-            <template #prefix>
-              <el-icon><User/></el-icon>
-            </template>
-          </el-input>
-          <el-input type="password" style="margin-top: 10px" placeholder="密码">
-            <template #prefix>
-              <el-icon><Lock/></el-icon>
-            </template>
-          </el-input>
-        </div>
-
-          <el-row style="margin-top: 5px">
-            <el-col :span="12" style="text-align: left">
-              <el-checkbox v-model="checked1" label="记住我"/>
-            </el-col>
-            <el-col :span="12" style="text-align: right">
-              <el-link>忘记密码？</el-link>
-            </el-col>
-          </el-row>
-
-        <div style="margin-top: 30px">
-          <el-button style="width: 250px" type="success" plain>登陆</el-button>
-        </div>
-        <el-divider>
-          <span style="color: grey;font-size: 13px">没有账号</span>
-        </el-divider>
-        <div>
-          <el-button style="width: 250px" type="warning" plain>注册账号</el-button>
-        </div>
-      </div>
-
-    </div>
-  </div>
+  <router-view/>
 </template>
 
 <style scoped>
---
+++ study-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="width: 100vw; height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1;background-color: dodgerblue">
+      <el-image style="width: 100%;height: 100%" fit="cover" src="https://img1.baidu.com/it/u=2463514011,1142503686&fm=253&fmt=auto&app=138&f=JPEG?w=888&h=500"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 50px;font-weight: bold">欢迎来我们的学习平台</div>
+      <div style="margin-top: 10px;font-size: 40px">Something wonderful is about to happen.</div>
+    </div>
+    <div style="width: 400px;background-color: white">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
