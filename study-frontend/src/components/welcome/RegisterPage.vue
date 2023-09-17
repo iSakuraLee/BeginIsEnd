9e889740a6aa5c06a5a928de186e08fc0fd65b70
sakura <1234@gmail.com>
2023-09-17 18:48:24 +0800

    用户名前端校验

--- study-frontend/src/components/welcome/RegisterPage.vue
+++ study-frontend/src/components/welcome/RegisterPage.vue
@@ -12,6 +12,24 @@ const form = reactive({
   email: '',
   verify_code: ''
 })
+
+const validateUsername = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('用户名为空，请重新输入'))
+  } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)) {
+    callback(new Error('用户名中不能包含特殊字符'))
+  } else {
+    callback()
+  }
+}
+
+const rules = {
+  username: [
+    {validator: validateUsername, trigger: ['blur','change']},
+    {min: 2, max: 8, message: '用户名长度需要在2-8个字符之间', trigger: ['blur', 'change']}
+  ]
+}
+
 </script>
 
 <template>
@@ -21,40 +39,50 @@ const form = reactive({
       <div style="font-size: 14px;color: grey">欢迎注册我们的学习平台</div>
     </div>
     <div class="register">
-      <el-input v-model="form.username" type="text" style="margin-top: 10px" placeholder="用户名">
-        <template #prefix>
-          <el-icon><User/></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.password" type="password" style="margin-top: 10px" placeholder="密码">
-        <template #prefix>
-          <el-icon><Lock/></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.password_repeat" type="password" style="margin-top: 10px" placeholder="确认密码">
-        <template #prefix>
-          <el-icon><Lock/></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.email" type="mail" style="margin-top: 10px" placeholder="电子邮箱地址">
-        <template #prefix>
-          <el-icon><message/></el-icon>
-        </template>
-      </el-input>
-    </div>
-    <div style="margin-top: 10px">
-      <el-row :gutter="10" style="width: 100%">
-        <el-col :span="17" style="width: 70%">
-          <el-input v-model="form.verify_code" type="mail" placeholder="请输入验证码">
+      <el-form :model="form" :rules="rules">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" type="text" placeholder="用户名">
             <template #prefix>
-              <el-icon><Ship/></el-icon>
+              <el-icon><User/></el-icon>
             </template>
           </el-input>
-        </el-col>
-        <el-col :span="6" style="width: 30%">
-          <el-button type="success">获取验证码</el-button>
-        </el-col>
-      </el-row>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="form.password" type="password" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock/></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password_repeat">
+          <el-input v-model="form.password_repeat" type="password" placeholder="确认密码">
+            <template #prefix>
+              <el-icon><Lock/></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="email">
+          <el-input v-model="form.email" type="mail" placeholder="电子邮箱地址">
+            <template #prefix>
+              <el-icon><message/></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="verify_code">
+          <el-row :gutter="10" style="width: 100%">
+            <el-col :span="17" style="width: 70%">
+              <el-input v-model="form.verify_code" type="mail" placeholder="请输入验证码">
+                <template #prefix>
+                  <el-icon><Ship/></el-icon>
+                </template>
+              </el-input>
+            </el-col>
+            <el-col :span="6" style="width: 30%">
+              <el-button type="success">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
     </div>
     <div style="margin-top: 70px">
       <el-button style="width: 270px; font-size: 18px" type="warning" plain>立即注册</el-button>
